fed00f67922b1f51fcb464ce8b6850b1b673cedc
felipemelonunes09 <felipe.melonunes09@gmail.com>
2022-01-14 17:39:27 -0300

    JWT secrets implemented

--- src/enviroment.js
+++ src/enviroment.js
@@ -5,9 +5,13 @@ require('dotenv')
 
 module.exports = {
     PORT: process.env.PORT || 8081,
+
     DB_HOST: process.env.DB_HOST || 'localhost',
     DB_NAME: process.env.DB_NAME || 'local_db_test',
     DB_PASSWORD: process.env.DB_PASSWORD || 'root',
     DB_USER: process.env.DB_PASSWORD || 'root',
-    DB_FORCE: (process.env.DB_FORCE == 'false' ? false : false) || false
+    DB_FORCE: (process.env.DB_FORCE == 'false' ? false : false) || false,
+
+    JWT_SECRET: (process.env.JWT_SECRET) || 'default_public_key',
+    JWT_EXPIRATION: (process.env.JWT_EXPIRATION) || '1h'
 }
