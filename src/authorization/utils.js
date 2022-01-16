f98094fa186bcf6c554f6889be8bb6b1dab44491
felipemelonunes09 <felipe.melonunes09@gmail.com>
2022-01-15 23:21:13 -0300

    dictionary file

---
+++ src/authorization/utils.js
@@ -0,0 +1,17 @@
+const AUTHORIZATION_DICIONATY = {
+    ERROR: -2,
+    REJECT: -1,
+    ACCEPTED: 0,
+    OK : 1,
+}
+
+const ROUTE_DICTIONARY = { 
+    NOT_MAPPED: 2,
+    PRIVATE: 3,
+    PUBLIC: 4
+}
+
+module.exports = { 
+    AUTHORIZATION_DICIONATY,
+    ROUTE_DICTIONARY
+}
